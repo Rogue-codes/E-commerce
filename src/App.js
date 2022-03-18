58593d012187391706da71a8fd4bea403316941e
Nnamdi Osuji <nnamdidanielosuji@gamil.com>
2022-03-18 16:22:59 +0100

    working

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Cart from "./Components/Cart";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
 import ProductPage from "./Components/ProductPage";
+import Nav from "./Components/Nav";
 
 
 function App() {
@@ -14,12 +15,13 @@ function App() {
 
     <div className="App">
     <Router>
+      <Nav/>
       <Routes>
-        <Route path="/" element={ <Home/>}/>
+        {/* <Route path="/" element={ <Home/>}/>
         <Route path="/login" element={ <Login/>}/>
         <Route path="/register" element={ <Register/>}/>
         <Route path="/prodpage" element={ <ProductPage/>}/>
-        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/cart" element={<Cart/>}/> */}
       </Routes>
     </Router>
       
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from "react-router-dom";
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch, faShoppingCart, faSign, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faSearch, faShoppingCart, faSign, faUser } from '@fortawesome/free-solid-svg-icons'
+import { useTransition, animated } from 'react-spring'
 
 const Components = styled.div`
+    @media (max-width:450px) {
+        justify-content: space-between;
+        padding: 4%;
+    }
     padding:2%; 
     width:100%;
     height:15vh;
@@ -17,9 +22,60 @@ const Components = styled.div`
     display:flex;
     justify-content: flex-start;
     align-items:center;
+    .firstAnimate{
+    @media (max-width:480px) {
+      display: block;
+    }
+    display: none;
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 50;
+    background-color: #010214a6;
+  }
+  .animate{
+    @media (max-width:480px) {
+      display: block;
+    }
+    display: none;
+    width: 80%;
+    box-shadow: 2px 2px 4px #010214a6;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 70;
+    background-color: black;
+    .menu{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 1.2rem;
+      height: 10vh;
+      color: $textColor;
+    }
+    a{
+      display: block;
+      height: 15vh;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      border-top: 1px solid $textColor;
+      padding: 2% 5%;
+      font-size: 1.5rem;
+      color: white;
+      border-bottom: 1px solid #fff;
+      text-decoration: none;
+    }
+  }
 `
 
 const Left = styled.div`
+    @media (max-width:450px) {
+        width: 50%;
+    }
     width:30%;
     display:flex;
     justify-content:center;
@@ -29,6 +85,9 @@ const Left = styled.div`
 
 const Center = styled.div`
     a{
+        @media (max-width:450px) {
+            font-size: 1.2rem;
+        }
         font-size: 2vw;
         text-decoration: none;
         color: white;
@@ -37,6 +96,9 @@ const Center = styled.div`
 `
 
 const Right = styled.div`
+@media (max-width:450px) {
+    display: none;
+}
 display:flex;
 justify-content:flex-end;
 margin-left: 25%;
@@ -110,15 +172,25 @@ align-items:center;
 `
 
 const Lang = styled.p`
+@media (max-width:450px) {
+    display: none;
+}
 font-size:1.2vw;
 `
 
 const Form = styled.div`
-display:flex;
-justify-content:center;
-align-items:center;
-width:80%;
+    @media (max-width:450px) {
+        display: none;
+    }
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    width:80%;
 input{
+    @media (max-width:450px) {
+        height:6vh;
+        width:100%;
+    }
     height:7vh;
     width:100%;
     border:solid 1px lightgrey;
@@ -144,6 +216,27 @@ button{
 `
 
 function Nav() {
+    const [showMenu, setShowMenu]= useState(false)
+
+    const maskTransitions = useTransition(showMenu, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+        reverse: showMenu,
+        delay: 200,
+        // config: config.molasses,
+        // onRest: () => set(!show),
+      })
+
+      const menuTransitions = useTransition(showMenu, {
+        from: { opacity: 0, transform: "translateX(-100%)"},
+        enter: { opacity: 1, transform: "translateX(0%)" },
+        leave: { opacity: 0, transform: "translateX(-100%)" },
+        reverse: showMenu,
+        delay: 200,
+         // config: config.molasses,
+        // onRest: () => set(!show),
+      })
   return (
     <Components>
         <Center>
@@ -182,6 +275,28 @@ function Nav() {
             <Link to='/cart'>Cart </Link>
             </div>
         </Right>
+
+        <span className='icon'>
+            <FontAwesomeIcon icon={faBars} onClick ={ () => setShowMenu(!showMenu) } style={{color:'white'}} size="xl"></FontAwesomeIcon>
+        </span>
+
+        {
+            maskTransitions(
+            (styles, item) => item && <animated.div style={styles} className='firstAnimate' onClick={()=> setShowMenu(false)}>
+
+            </animated.div>
+            )
+        }
+
+        {
+            menuTransitions(
+            (styles, item) => item && <animated.div style={styles} className='animate'>
+                <Link to='/login' onClick={()=> setShowMenu(false)}>Login</Link>
+                <Link to='/register' onClick={()=> setShowMenu(false)}>Sign in</Link>
+                <Link to='/cart' onClick={()=> setShowMenu(false)}>Cart</Link>
+            </animated.div>
+            )
+        }
     </Components>
   )
 }
